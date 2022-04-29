f43541a5ad21fda340c3385bec8b112caeda3358
Lenovo35 <arabsamir2003@gmail.com>
2022-04-29 10:34:24 -0300

    Exercise 4

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,5 @@
 fun main() {
-    eje3()
+    eje4()
 }
 fun eje1(name: String){
     print("Hola $name!")
@@ -23,4 +23,15 @@ fun eje3(){
 
     var avg = (a+b+c)/3
     print("El promedio de alturas es: $avg")
+}
+fun eje4(){
+    println("Ingrese el ancho de un rectángulo:")
+    var base = readLine()!!.toInt()
+    println("Ingrese el alto de ese rectángulo:")
+    var alt = readLine()!!.toInt()
+
+    var peri = base*2 + alt*2
+    var area = base * alt
+
+    print("El perímetro del rectángulo es $peri y el área es $area")
 }
